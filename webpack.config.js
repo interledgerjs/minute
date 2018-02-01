dd625cce1352444e9831d05b1bd6095620dca585
Ben Sharafian <benms64@gmail.com>
2018-02-01 11:46:20 -0800

    feat: nicer looking popup

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 'use strict'
+const webpack = require('webpack')
 
 module.exports = {
   entry: {
     'dist/websocket': './src/polyfill/websocket.js',
     'dist/background': './src/background.js',
     'dist/content': './src/content.js',
-    'dist/inject': './src/inject.js'
+    'dist/inject': './src/inject.js',
+    'dist/popup': './src/popup.js'
   },
 
   output: {
@@ -36,10 +38,31 @@ module.exports = {
           ]
         }
       },
+      {
+        test: /\.css$/,
+        loaders: ["style-loader", "css-loader"]
+      },
       { test: /ed25519/, loader: 'null' },
-      { test: /\.json$/, loader: 'json-loader' }
+      { test: /\.json$/, loader: 'json-loader' },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]"
+      },{
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader?name=fonts/[name].[ext]"
+      },{
+        test: /\.(jpe?g|png|gif)$/,
+        loader:'file-loader?name=img/[name].[ext]'
+      }
     ]
   },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    })
+  ],
 
   node: {
     console: true,
